8f467773ef8313817e583ea7917f2bd3fa9a039b
liuwenzhi1111 <liuwz89@outlook.com>
2018-06-29 11:11:08 +0800

    第二次测试

---
+++ test01/test_1.py
@@ -0,0 +1,14 @@
+import pytest
+# class Test:
+
+# class Test:
+    # @pytest.mark.parametrize("username",["lisi"])
+    # def test_login(self,username):
+    #     self.name=username
+    #     print(self.name)
+    # @pytest.mark.parametrize(("username","passwd"),[("lisi","123456")])
+    # def test_login2(self,username,passwd):
+    #     self.name=username
+    #     self.passwd=passwd
+    #     print(self.name,self.passwd)
+
